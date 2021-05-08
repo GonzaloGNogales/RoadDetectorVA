5084ea430837d84e202c9facf902434922248dcb
GonzaloGNogales <gonzalognbusiness@gmail.com>
2021-05-08 18:39:23 +0200

    Crop region adjusting and offset

--- MSERDetector/mser_detector.py
+++ MSERDetector/mser_detector.py
@@ -7,7 +7,7 @@ from DetectorUtilities.region import *
 
 
 class MSER_Detector:
-    def __init__(self, delta=10, max_variation=0.25, max_area=1000, min_area=50):
+    def __init__(self, delta=7, max_variation=0.2, max_area=2000, min_area=50):
         self.original_images = {}   # Original map for saving the original data (color detection)
         self.greyscale_images = {}  # Map containing Greyscale images to feed MSER Detector (localization with MSER)
         self.ground_truth = {}      # Map containing the regions of the present signals in the training images
@@ -41,17 +41,19 @@ class MSER_Detector:
             self.greyscale_images[key] = (cv2.cvtColor(self.original_images[key], cv2.COLOR_BGR2GRAY))
 
     # DETECTOR TRAINING
-    def fit(self, max_value=255, adaptive_method=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_type=cv2.THRESH_BINARY, block_size=7, c=12):
+    def fit(self):
         mser_outputs = {}      # Output map containing MSER detected regions
         training_output = {}   # Map for storing the outputs of the training phase, for visualization purposes
 
         for key in self.greyscale_images:
-            # 2 - Filter with adaptive threshold for increasing the contrast of the points of interest
-            # self.greyscale_images[key] = cv2.adaptiveThreshold(self.greyscale_images[key], max_value, adaptive_method, threshold_type, block_size, c)
+            # 2 - Filter equalizing the histogram for increasing the contrast of the points of interest
+            # equalized_greyscale_image = cv2.equalizeHist(self.greyscale_images[key])
             mser_outputs[key] = (np.zeros((self.original_images[key].shape[0], self.original_images[key].shape[1], 3), dtype=np.uint8))
 
             # Detect polygons (regions) from the image
             regions, _ = self.mser.detectRegions(self.greyscale_images[key])
+            # regions_equalized, _ = self.mser.detectRegions(equalized_greyscale_image)
+            # regions = regions_non_equalized + regions_equalized
 
             # Color MSER output **************************************************************************************
             for region in regions:
@@ -61,25 +63,25 @@ class MSER_Detector:
             # ********************************************************************************************************
 
             # Color rectangles and Mask extraction
-            candidate_regions = list()
             filtered_detected_regions = list()
             masks = list()
             original_image = np.copy(self.original_images[key])
             for region in regions:
                 x, y, w, h = cv2.boundingRect(region)
-                if abs(1 - w / h) <= 0.8:
+                if abs(1 - w / h) <= 0.5:
                     color_RGB = hsv_to_rgb(random(), 1, 1)  # Generate a random color
                     color_RGB = tuple(int(color * 255) for color in color_RGB)
-                    x -= 5
-                    y -= 5
+
+                    # Adjust the width and height to obtain a perfect square for the region
+                    x = max(x - 5, 0)
+                    y = max(y - 5, 0)
                     w += 10
                     h += 10
-                    candidate_regions.append(region)  # Save polygon into candidate regions
-                    crop_img = original_image[y:y + h, x:x + w]  # crop_img = self.original_images[i][y:y + h, x:x + w]
-                    h, w, _ = crop_img.shape
-
-                    if h <= 0 or w <= 0:  # Control test for not getting inconsistent dimensions
-                        continue
+                    if w > h:
+                        h = w
+                    elif h > w:
+                        w = h
+                    crop_region = original_image[y:y + h, x:x + w]
 
                     # Red levels in HSV approximation
                     low_red_1 = np.array([0, 100, 20])
@@ -88,7 +90,7 @@ class MSER_Detector:
                     high_red_2 = np.array([179, 255, 255])
 
                     # Red mask creation for HSV color thresholding
-                    crop_img_HSV = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+                    crop_img_HSV = cv2.cvtColor(crop_region, cv2.COLOR_BGR2HSV)
                     red_mask_1 = cv2.inRange(crop_img_HSV, low_red_1, high_red_1)
                     red_mask_2 = cv2.inRange(crop_img_HSV, low_red_2, high_red_2)
                     red_mask = cv2.add(red_mask_1, red_mask_2)
@@ -97,7 +99,7 @@ class MSER_Detector:
                     red_mask_mean = np.mean(red_mask)
                     red_mask = cv2.resize(red_mask, (25, 25))
                     if 20 < red_mask_mean < 80:
-                        filtered_detected_regions.append(crop_img)
+                        filtered_detected_regions.append(crop_region)
                         masks.append(red_mask)
 
                     # *********************************************** DEBUG ********************************************
@@ -105,7 +107,7 @@ class MSER_Detector:
                     # we only want the regions not painting the rectangles for the final version
                     # **************************************************************************************************
 
-            training_output[key] = (masks, filtered_detected_regions, mser_outputs, self.original_images[key])
+            training_output[key] = (masks, filtered_detected_regions, mser_outputs, self.original_images[key], self.greyscale_images[key])
 
         return training_output
 
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     # Create the detector
     if vars(args)['detector'] == 'mser':
-        detector = MSER_Detector(delta=15)
+        detector = MSER_Detector()
 
         # Load training data
         detector.preprocess_data(vars(args)['train_path'])
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         # DEBUG OR VISUALIZE TRAINING RESULTS
         visualizer = Detector_Visualizer(training_results)
         visualizer.setup_directory()
-        visualizer.show_and_save()
+        visualizer.save_to_dir()
 
         # NOT IMPLEMENTED YET
         # Load testing data
